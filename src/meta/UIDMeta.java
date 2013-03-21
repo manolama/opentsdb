f0c6f2325ccf3f382afb035907cbba6b1be5e2c7
clarsen <clarsen@euphoriaaudio.com>
2013-03-21 18:17:57 -0400

    Add missing headers

--- src/meta/UIDMeta.java
+++ src/meta/UIDMeta.java
@@ -1,3 +1,15 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2010-2012  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
 package net.opentsdb.meta;
 
 import java.util.HashMap;
@@ -6,10 +18,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * UIDMeta objects are associated with the {@link UniqueId} of metrics, tag names
+ * UIDMeta objects are associated with the {@link #UniqueId} of metrics, tag names
  * or tag values. When a new metric, tagk or tagv is generated, a  UIDMeta object
- * will also be written to storage with only the {@link uid}, {@link type} and
- * {@link name} filled out. Users can then modify mutable fields
+ * will also be written to storage with only the {@link #uid}, {@link #type} and
+ * {@link #name} filled out. Users can then modify mutable fields
+ * @since 2.0
  */
 public class UIDMeta {
   protected static final Logger LOG = LoggerFactory.getLogger(UIDMeta.class);
