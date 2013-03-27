89a69b9a6532c4ac563c5d2c8d7ac56853a9ce6e
clarsen <clarsen@euphoriaaudio.com>
2013-03-27 00:55:23 -0400

    TSMeta update

--- src/meta/TSMeta.java
+++ src/meta/TSMeta.java
@@ -15,22 +15,18 @@ package net.opentsdb.meta;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Timeseries Metadata is associated with a particular series of data points
  * and includes user configurable values and some stats calculated by OpenTSDB.
  * Whenever a new timeseries is recorded, an associated TSMeta object will
- * be recorded with only the {@tsuid} field configured.
- * 
+ * be recorded with only the tsuid field configured.
+ * <p>
  * The metric and tag UIDMeta objects are loaded from their respective locations
  * in the data storage system.
  * @since 2.0
  */
 public class TSMeta {
-  private static final Logger LOG = LoggerFactory.getLogger(TSMeta.class);
-  
+
   /** Hexadecimal representation of the TSUID this metadata is associated with */
   private String tsuid;
   
@@ -76,9 +72,147 @@ public class TSMeta {
    */
   private double min; 
   
-  /** A system recorded value of how often this data is recorded */
-  private double interval;
-  
   /** The last time this data was recorded in seconds */
   private double last_received;
+
+  /** @return the tsuid */
+  public final String getTSUID() {
+    return tsuid;
+  }
+
+  /** @return the metric UID meta object */
+  public final UIDMeta getMetric() {
+    return metric;
+  }
+
+  /** @return the tag UID meta objects in an array, tagk first, then tagv, etc */
+  public final ArrayList<UIDMeta> getTags() {
+    return tags;
+  }
+
+  /** @return the display name */
+  public final String getDisplayName() {
+    return display_name;
+  }
+
+  /** @return the description */
+  public final String getDescription() {
+    return description;
+  }
+
+  /** @return the notes */
+  public final String getNotes() {
+    return notes;
+  }
+
+  /** @return the created */
+  public final long getCreated() {
+    return created;
+  }
+
+  /** @return the custom key/value map, may be null */
+  public final HashMap<String, String> getCustom() {
+    return custom;
+  }
+
+  /** @return the units */
+  public final String getUnits() {
+    return units;
+  }
+
+  /** @return the data type */
+  public final String getDataType() {
+    return data_type;
+  }
+
+  /** @return the retention */
+  public final int getRetention() {
+    return retention;
+  }
+
+  /** @return the max value */
+  public final double getMax() {
+    return max;
+  }
+
+  /** @return the min value*/
+  public final double getMin() {
+    return min;
+  }
+
+  /** @return the last received timestamp */
+  public final double getLastReceived() {
+    return last_received;
+  }
+
+  /** @param tsuid the tsuid to set */
+  public final void setTSUID(final String tsuid) {
+    this.tsuid = tsuid;
+  }
+
+  /** @param metric the metric UID meta object */
+  public final void setMetric(final UIDMeta metric) {
+    this.metric = metric;
+  }
+
+  /** @param tags the tag UID meta objects. Must be an array starting with a 
+   * tagk object followed by the associataed tagv. */
+  public final void setTags(final ArrayList<UIDMeta> tags) {
+    this.tags = tags;
+  }
+
+  /** @param display_name the display name to set */
+  public final void setDisplayName(final String display_name) {
+    this.display_name = display_name;
+  }
+
+  /** @param description the description to set */
+  public final void setDescription(final String description) {
+    this.description = description;
+  }
+
+  /** @param notes the notes to set */
+  public final void setNotes(final String notes) {
+    this.notes = notes;
+  }
+
+  /** @param created the created to set */
+  public final void setCreated(final long created) {
+    this.created = created;
+  }
+
+  /** @param custom the custom to set */
+  public final void setCustom(final HashMap<String, String> custom) {
+    this.custom = custom;
+  }
+
+  /** @param units the units to set */
+  public final void setUnits(final String units) {
+    this.units = units;
+  }
+
+  /** @param data_type the data type to set */
+  public final void setDataType(final String data_type) {
+    this.data_type = data_type;
+  }
+
+  /** @param retention the retention to set */
+  public final void setRetention(final int retention) {
+    this.retention = retention;
+  }
+
+  /** @param max the max to set */
+  public final void setMax(final double max) {
+    this.max = max;
+  }
+
+  /** @param min the min to set */
+  public final void setMin(final double min) {
+    this.min = min;
+  }
+
+  /** @param last_received the last received timestamp */
+  public final void setLastReceived(final double last_received) {
+    this.last_received = last_received;
+  }
 }
