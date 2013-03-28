2fc8860165af8c1eb3c0c28bb35e42817ab6e329
clarsen <clarsen@euphoriaaudio.com>
2013-03-28 14:26:59 -0400

    Add NettyMocks and TestHTTPQuery to the makefile Add missing header to NettyMocks.java TestGraphHandler uses NettyMocks now Header fix for TestHttpQuery.java

--- test/tsd/TestHttpQuery.java
+++ test/tsd/TestHttpQuery.java
@@ -1,11 +1,26 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2011-2012  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
 package net.opentsdb.tsd;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +38,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ HttpQuery.class })
+@PrepareForTest(HttpQuery.class)
 public class TestHttpQuery {
  
   @Test
@@ -336,7 +351,7 @@ public class TestHttpQuery {
   
   @Test (expected = IllegalArgumentException.class)
   public void guessMimeTypeFromUriNull() throws Exception {
-    ReflectguessMimeTypeFromUri().invoke(null, null);
+    ReflectguessMimeTypeFromUri().invoke(null, (Object[])null);
   }
   
   @Test 
