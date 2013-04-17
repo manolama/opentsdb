fe88db58aee37062bb1434ba342df7ec64bee74f
clarsen <clarsen@euphoriaaudio.com>
2013-04-16 21:06:01 -0400

    fix unnecessary imports

--- src/core/TSDB.java
+++ src/core/TSDB.java
@@ -12,8 +12,6 @@
 // see <http://www.gnu.org/licenses/>.
 package net.opentsdb.core;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
