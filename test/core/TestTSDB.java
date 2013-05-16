597ca3a4c9947beaa0ea404dede60c64805a4cf0
clarsen <clarsen@euphoriaaudio.com>
2013-05-15 22:37:36 -0400

    test/core/TestTSDB.java

--- test/core/TestTSDB.java
+++ test/core/TestTSDB.java
@@ -31,11 +31,15 @@ import org.hbase.async.HBaseClient;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.stumbleupon.async.Deferred;
 
+@PowerMockIgnore({"javax.management.*", "javax.xml.*",
+  "ch.qos.*", "org.slf4j.*",
+  "com.sum.*", "org.xml.*"})
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({TSDB.class, Config.class, UniqueId.class, HBaseClient.class, 
   CompactionQueue.class})
