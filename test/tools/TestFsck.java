2fd08d2e07432389b5f40c402c1e2a62232978b5
clarsen <clarsen@euphoriaaudio.com>
2013-06-22 18:01:17 -0400

    TEMP TestFsck.java

---
+++ test/tools/TestFsck.java
@@ -0,0 +1,116 @@
+package net.opentsdb.tools;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import net.opentsdb.core.TSDB;
+import net.opentsdb.meta.Annotation;
+import net.opentsdb.storage.MockBase;
+import net.opentsdb.uid.NoSuchUniqueName;
+import net.opentsdb.uid.UniqueId;
+import net.opentsdb.utils.Config;
+
+import org.apache.zookeeper.proto.DeleteRequest;
+import org.hbase.async.GetRequest;
+import org.hbase.async.HBaseClient;
+import org.hbase.async.KeyValue;
+import org.hbase.async.PutRequest;
+import org.hbase.async.Scanner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.stumbleupon.async.Deferred;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore({"javax.management.*", "javax.xml.*",
+  "ch.qos.*", "org.slf4j.*",
+  "com.sum.*", "org.xml.*"})
+@PrepareForTest({TSDB.class, Config.class, UniqueId.class, HBaseClient.class, 
+  GetRequest.class, PutRequest.class, KeyValue.class, Fsck.class,
+  Scanner.class, DeleteRequest.class, Annotation.class })
+public final class TestFsck {
+  private Config config;
+  private TSDB tsdb = null;
+  private HBaseClient client = mock(HBaseClient.class);
+  private UniqueId metrics = mock(UniqueId.class);
+  private UniqueId tag_names = mock(UniqueId.class);
+  private UniqueId tag_values = mock(UniqueId.class);
+  private MockBase storage;
+ 
+  private final static Method fsck;
+  static {
+    try {
+      fsck = Fsck.class.getDeclaredMethod("fsck", TSDB.class, HBaseClient.class, 
+          byte[].class, boolean.class, String[].class);
+      fsck.setAccessible(true);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed in static initializer", e);
+    }
+  }
+  
+  @Before
+  public void before() throws Exception {
+    config = new Config(false);
+    tsdb = new TSDB(config);
+    
+    storage = new MockBase(tsdb, client, true, true, true, true);
+    storage.setFamily("t".getBytes(MockBase.ASCII()));
+    
+    // replace the "real" field objects with mocks
+    Field cl = tsdb.getClass().getDeclaredField("client");
+    cl.setAccessible(true);
+    cl.set(tsdb, client);
+    
+    Field met = tsdb.getClass().getDeclaredField("metrics");
+    met.setAccessible(true);
+    met.set(tsdb, metrics);
+    
+    Field tagk = tsdb.getClass().getDeclaredField("tag_names");
+    tagk.setAccessible(true);
+    tagk.set(tsdb, tag_names);
+    
+    Field tagv = tsdb.getClass().getDeclaredField("tag_values");
+    tagv.setAccessible(true);
+    tagv.set(tsdb, tag_values);
+    
+    // mock UniqueId
+    when(metrics.getId("sys.cpu.user")).thenReturn(new byte[] { 0, 0, 1 });
+    when(metrics.getName(new byte[] { 0, 0, 1 })).thenReturn("sys.cpu.user");
+    when(metrics.getId("sys.cpu.system"))
+      .thenThrow(new NoSuchUniqueName("sys.cpu.system", "metric"));
+    when(metrics.getId("sys.cpu.nice")).thenReturn(new byte[] { 0, 0, 2 });
+    when(metrics.getName(new byte[] { 0, 0, 2 })).thenReturn("sys.cpu.nice");
+    when(tag_names.getId("host")).thenReturn(new byte[] { 0, 0, 1 });
+    when(tag_names.getName(new byte[] { 0, 0, 1 })).thenReturn("host");
+    when(tag_names.getOrCreateId("host")).thenReturn(new byte[] { 0, 0, 1 });
+    when(tag_names.getId("dc")).thenThrow(new NoSuchUniqueName("dc", "metric"));
+    when(tag_values.getId("web01")).thenReturn(new byte[] { 0, 0, 1 });
+    when(tag_values.getName(new byte[] { 0, 0, 1 })).thenReturn("web01");
+    when(tag_values.getOrCreateId("web01")).thenReturn(new byte[] { 0, 0, 1 });
+    when(tag_values.getId("web02")).thenReturn(new byte[] { 0, 0, 2 });
+    when(tag_values.getName(new byte[] { 0, 0, 2 })).thenReturn("web02");
+    when(tag_values.getOrCreateId("web02")).thenReturn(new byte[] { 0, 0, 2 });
+    when(tag_values.getId("web03"))
+      .thenThrow(new NoSuchUniqueName("web03", "metric"));
+    
+    when(metrics.width()).thenReturn((short)3);
+    when(tag_names.width()).thenReturn((short)3);
+    when(tag_values.width()).thenReturn((short)3);
+  }
+  
+  @Test
+  public void fsk() throws Exception {
+    fsck.invoke(null, tsdb, client, "tsdb".getBytes(MockBase.ASCII()), false, 
+        new String[] { "1356998400", "1357002000", "sum", "sys.cpu.user" });
+  }
+}
