d51f9fb4e6d7792b275310207d3afd3169e6e417
clarsen <clarsen@euphoriaaudio.com>
2013-08-01 21:21:45 -0400

    Add async methods for all DataPoints implementations so that the full query path can be converted to async code.

--- src/core/RowSeq.java
+++ src/core/RowSeq.java
@@ -27,6 +27,8 @@ import org.slf4j.LoggerFactory;
 import org.hbase.async.Bytes;
 import org.hbase.async.KeyValue;
 
+import com.stumbleupon.async.Deferred;
+
 /**
  * Represents a read-only sequence of continuous HBase rows.
  * <p>
@@ -228,18 +230,43 @@ final class RowSeq implements DataPoints {
   }
 
   public String metricName() {
+    try {
+      return metricNameAsync().joinUninterruptibly();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException("Should never be here", e);
+    }
+  }
+
+  public Deferred<String> metricNameAsync() {
     if (key == null) {
       throw new IllegalStateException("the row key is null!");
     }
-    return RowKey.metricName(tsdb, key);
+    return RowKey.metricNameAsync(tsdb, key);
   }
-
+  
   public Map<String, String> getTags() {
-    return Tags.getTags(tsdb, key);
+    try {
+      return getTagsAsync().joinUninterruptibly();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException("Should never be here", e);
+    }
+  }
+  
+  public Deferred<Map<String, String>> getTagsAsync() {
+    return Tags.getTagsAsync(tsdb, key);
   }
 
   public List<String> getAggregatedTags() {
     return Collections.emptyList();
+  }
+  
+  public Deferred<List<String>> getAggregatedTagsAsync() {
+    final List<String> empty = Collections.emptyList();
+    return Deferred.fromResult(empty);
   }
   
   public List<String> getTSUIDs() {
