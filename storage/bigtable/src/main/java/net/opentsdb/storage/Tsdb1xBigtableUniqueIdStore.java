664a3f640c938797646b76c294c33902a00b0b76
Chris Larsen <clarsen@yahoo-inc.com>
2018-07-21 12:07:40 -0700

    updating

--- storage/bigtable/src/main/java/net/opentsdb/storage/Tsdb1xBigtableUniqueIdStore.java
+++ storage/bigtable/src/main/java/net/opentsdb/storage/Tsdb1xBigtableUniqueIdStore.java
@@ -17,6 +17,7 @@ package net.opentsdb.storage;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.LongAdder;
 
 import com.google.bigtable.v2.ReadRowsRequest;
@@ -24,6 +25,7 @@ import com.google.bigtable.v2.Row;
 import com.google.bigtable.v2.RowFilter;
 import com.google.bigtable.v2.RowSet;
 import com.google.cloud.bigtable.util.ByteStringer;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -37,10 +39,12 @@ import org.slf4j.LoggerFactory;
 
 import net.opentsdb.auth.AuthState;
 import net.opentsdb.core.Const;
+import net.opentsdb.core.TSDB;
 import net.opentsdb.data.TimeSeriesDatumId;
 import net.opentsdb.data.TimeSeriesId;
 import net.opentsdb.stats.Span;
 import net.opentsdb.uid.IdOrError;
+import net.opentsdb.uid.UniqueIdAssignmentAuthorizer;
 import net.opentsdb.uid.UniqueIdStore;
 import net.opentsdb.uid.UniqueIdType;
 import net.opentsdb.utils.Bytes;
@@ -56,9 +60,28 @@ import net.opentsdb.utils.Bytes;
 public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
   private static final Logger LOG = LoggerFactory.getLogger(
       Tsdb1xBigtableUniqueIdStore.class);
+
+  /** A map of the various config UID types. */
+  protected static final Map<UniqueIdType, String> CONFIG_PREFIX = 
+      Maps.newHashMapWithExpectedSize(UniqueIdType.values().length);
+  static {
+    for (final UniqueIdType type : UniqueIdType.values()) {
+      CONFIG_PREFIX.put(type, "uid." + type.toString().toLowerCase() + ".");
+    }
+  }
   
+  /** The error message returned when an ID is being assigned in the
+   * background and should be retried later. */
+  public static final String ASSIGN_AND_RETRY = 
+      "Assigning ID, queue and retry the data.";
+  
+  /** Various configuration keys. */
   public static final String CHARACTER_SET_KEY = "character_set";
   public static final String CHARACTER_SET_DEFAULT = "ISO-8859-1";
+  public static final String ASSIGN_AND_RETRY_KEY = "assign_and_retry";
+  public static final String RANDOM_ASSIGNMENT_KEY = "assign_random";
+  public static final String RANDOM_ATTEMPTS_KEY = "attempts.max_random";
+  public static final String ATTEMPTS_KEY = "attempts.max";
   
   public static final byte[] METRICS_QUAL = 
       "metrics".getBytes(Const.ASCII_CHARSET);
@@ -71,52 +94,89 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
   public static final byte[] ID_FAMILY = "id".getBytes(Const.ASCII_CHARSET);
   /** The single column family used by this class. */
   public static final byte[] NAME_FAMILY = "name".getBytes(Const.ASCII_CHARSET);
+  /** Row key of the special row used to track the max ID already assigned. */
+  public static final byte[] MAXID_ROW = { 0 };
+  /** How many time do we try to assign an ID before giving up. */
+  public static final short DEFAULT_ATTEMPTS_ASSIGN_ID = 3;
+//  /** How many time do we try to apply an edit before giving up. */
+//  public static final short MAX_ATTEMPTS_PUT = 6;
+  /** How many time do we try to assign a random ID before giving up. */
+  public static final short DEFAULT_ATTEMPTS_ASSIGN_RANDOM_ID = 10;
+//  /** Initial delay in ms for exponential backoff to retry failed RPCs. */
+//  private static final short INITIAL_EXP_BACKOFF_DELAY = 800;
+//  /** Maximum number of results to return in suggest(). */
+//  private static final short MAX_SUGGESTIONS = 25;
+  
   private final Tsdb1xBigtableDataStore data_store; 
+
+  /** The authorizer pulled from the registry. */
+  protected final UniqueIdAssignmentAuthorizer authorizer;
   
-  private final Charset character_set;
+  /** Character sets for each type. */
+  protected final Charset metric_character_set;
+  protected final Charset tagk_character_set;
+  protected final Charset tagv_character_set;
+
+  /** Whether or not to immediately return a RETRY state and run the 
+   * assignment process in the background. */
+  protected final boolean assign_and_retry;
   
-  private final Map<UniqueIdType, LongAdder> rejected_assignments;
+  /** Max attempts. */
+  protected final short max_attempts_assign;
+  protected final short max_attempts_assign_random;
+  
+  /** Whethe ror not to randomize UID assignments for these types. */
+  protected final boolean randomize_metric_ids;
+  protected final boolean randomize_tagk_ids;
+  protected final boolean randomize_tagv_ids;
   
   private final Map<UniqueIdType, Map<String, Deferred<byte[]>>> pending_assignments;
 
   public Tsdb1xBigtableUniqueIdStore(final Tsdb1xBigtableDataStore data_store) {
-    this.data_store = data_store;
-    if (!data_store.tsdb().getConfig().hasProperty(
-        data_store.getConfigKey(CHARACTER_SET_KEY))) {
-      data_store.tsdb().getConfig().register(
-          data_store.getConfigKey(CHARACTER_SET_KEY), 
-          CHARACTER_SET_DEFAULT, 
-          false, 
-          "The character set used for encoding/decoding all strings "
-              + "to UIDs.");
+    if (data_store == null) {
+      throw new IllegalArgumentException("Data store cannot be null.");
     }
-    character_set = Charset.forName(data_store.tsdb().getConfig()
-        .getString(data_store.getConfigKey(CHARACTER_SET_KEY)));
+    this.data_store = data_store;
+    registerConfigs(data_store.tsdb());
     
-    rejected_assignments = Maps.newHashMapWithExpectedSize(
-        UniqueIdType.values().length);
+    authorizer = data_store.tsdb().getRegistry()
+        .getDefaultPlugin(UniqueIdAssignmentAuthorizer.class);
+    
+    metric_character_set = Charset.forName(data_store.tsdb().getConfig()
+        .getString(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.METRIC) + CHARACTER_SET_KEY)));
+    tagk_character_set = Charset.forName(data_store.tsdb().getConfig()
+        .getString(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.TAGK) + CHARACTER_SET_KEY)));
+    tagv_character_set = Charset.forName(data_store.tsdb().getConfig()
+        .getString(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.TAGV) + CHARACTER_SET_KEY)));
+    
+    randomize_metric_ids = data_store.tsdb().getConfig()
+        .getBoolean(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.METRIC) + RANDOM_ASSIGNMENT_KEY));
+    randomize_tagk_ids = data_store.tsdb().getConfig()
+        .getBoolean(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.TAGK) + RANDOM_ASSIGNMENT_KEY));
+    randomize_tagv_ids = data_store.tsdb().getConfig()
+        .getBoolean(data_store.getConfigKey(
+            CONFIG_PREFIX.get(UniqueIdType.TAGV) + RANDOM_ASSIGNMENT_KEY));
+    
+    assign_and_retry = data_store.tsdb().getConfig()
+        .getBoolean(data_store.getConfigKey(ASSIGN_AND_RETRY_KEY));
+    max_attempts_assign = (short) data_store.tsdb().getConfig()
+        .getInt(data_store.getConfigKey(ATTEMPTS_KEY));
+    max_attempts_assign_random = (short) data_store.tsdb().getConfig()
+        .getInt(data_store.getConfigKey(RANDOM_ATTEMPTS_KEY));
+    
+    // It's important to create maps here.
     pending_assignments = Maps.newHashMapWithExpectedSize(
         UniqueIdType.values().length);
-    
     for (final UniqueIdType type : UniqueIdType.values()) {
-      rejected_assignments.put(type, new LongAdder());
       pending_assignments.put(type, Maps.newConcurrentMap());
     }
-//    this.client = client;
-//    this.table = table;
-//    if (kind.isEmpty()) {
-//      throw new IllegalArgumentException("Empty string as 'kind' argument!");
-//    }
-//    this.kind = toBytes(kind);
-//    type = stringToUniqueIdType(kind);
-//    if (width < 1 || width > 8) {
-//      throw new IllegalArgumentException("Invalid width: " + width);
-//    }
-//    this.id_width = (short) width;
-//    this.randomize_id = randomize_id;
     
-    LOG.info("Initalized UniqueId store with characterset: " 
-        + character_set);
+    LOG.info("Initalized UniqueId store.");
   }
 
   @Override
@@ -182,7 +242,7 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
         
         deferred.callback(results.isEmpty() ? null :
           new String(results.get(0).getFamilies(0).getColumns(0)
-              .getCells(0).getValue().toByteArray(), character_set));
+              .getCells(0).getValue().toByteArray(), characterSet(type)));
       }
 
       @Override
@@ -289,7 +349,7 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
             }
             names.add(new String(results.get(i).getFamilies(0).getColumns(0)
                 .getCells(0).getValue().toByteArray(), 
-                character_set));
+                characterSet(type)));
           }
         }
         
@@ -369,7 +429,7 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
     ReadRowsRequest request = ReadRowsRequest.newBuilder()
         .setTableNameBytes(ByteStringer.wrap(data_store.uid_table))
         .setRows(RowSet.newBuilder()
-            .addRowKeys(ByteStringer.wrap(name.getBytes(character_set))))
+            .addRowKeys(ByteStringer.wrap(name.getBytes(characterSet(type)))))
         .setFilter(RowFilter.newBuilder()
             .setFamilyNameRegexFilterBytes(ByteStringer.wrap(ID_FAMILY))
             .setColumnQualifierRegexFilter(ByteStringer.wrap(qualifier))
@@ -464,7 +524,7 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
         throw new IllegalArgumentException("A null or empty name was "
             + "found in the list.");
       }
-      rows.addRowKeys(ByteStringer.wrap(name.getBytes(character_set)));
+      rows.addRowKeys(ByteStringer.wrap(name.getBytes(characterSet(type))));
     }
     
     final Deferred<List<byte[]>> deferred = new Deferred<List<byte[]>>();
@@ -492,7 +552,7 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
               results.get(i).getFamilies(0).getColumnsCount() < 1) {
             continue;
           } else {
-            while (!new String(results.get(i).getKey().toByteArray(), character_set)
+            while (!new String(results.get(i).getKey().toByteArray(), characterSet(type))
                 .equals(names.get(name_idx++))) {
               ids.add(null);
             }
@@ -546,14 +606,84 @@ public class Tsdb1xBigtableUniqueIdStore implements UniqueIdStore {
   @Override
   public Deferred<List<IdOrError>> getOrCreateIds(AuthState auth,
       UniqueIdType type, List<String> names, TimeSeriesDatumId id, Span span) {
-    // TODO Auto-generated method stub
+    
     return null;
   }
 
+
   @Override
-  public Charset characterSet(UniqueIdType type) {
-    // TODO Auto-generated method stub
-    return null;
+  public Charset characterSet(final UniqueIdType type) {
+    switch (type) {
+    case METRIC:
+      return metric_character_set;
+    case TAGK:
+      return tagk_character_set;
+    case TAGV:
+      return tagv_character_set;
+    default:
+      throw new IllegalArgumentException("Unsupported type: " + type);
+    }
+  }
+
+  @VisibleForTesting
+  void registerConfigs(final TSDB tsdb) {
+    for (final Entry<UniqueIdType, String> entry : CONFIG_PREFIX.entrySet()) {
+      if (!data_store.tsdb().getConfig().hasProperty(
+          data_store.getConfigKey(entry.getValue() + CHARACTER_SET_KEY))) {
+        data_store.tsdb().getConfig().register(
+            data_store.getConfigKey(entry.getValue() + CHARACTER_SET_KEY), 
+            CHARACTER_SET_DEFAULT, 
+            false, 
+            "The character set used for encoding/decoding UID "
+            + "strings for " + entry.getKey().toString().toLowerCase() 
+            + " entries.");
+      }
+      
+      if (!data_store.tsdb().getConfig().hasProperty(
+          data_store.getConfigKey(entry.getValue() + RANDOM_ASSIGNMENT_KEY))) {
+        data_store.tsdb().getConfig().register(
+            data_store.getConfigKey(entry.getValue() + RANDOM_ASSIGNMENT_KEY), 
+            false, 
+            false, 
+            "Whether or not to randomly assign UIDs for " 
+            + entry.getKey().toString().toLowerCase() + " entries "
+                + "instead of incrementing a counter in storage.");
+      }
+    }
+    
+    if (!data_store.tsdb().getConfig().hasProperty(
+        data_store.getConfigKey(ASSIGN_AND_RETRY_KEY))) {
+      data_store.tsdb().getConfig().register(
+          data_store.getConfigKey(ASSIGN_AND_RETRY_KEY), 
+          false, 
+          false, 
+          "Whether or not to return with a RETRY write state immediately "
+          + "when a UID needs to be assigned and being the assignment "
+          + "process in the background. The next time the same string "
+          + "is looked up it should be assigned.");
+    }
+    
+    if (!data_store.tsdb().getConfig().hasProperty(
+        data_store.getConfigKey(RANDOM_ATTEMPTS_KEY))) {
+      data_store.tsdb().getConfig().register(
+          data_store.getConfigKey(RANDOM_ATTEMPTS_KEY), 
+          DEFAULT_ATTEMPTS_ASSIGN_RANDOM_ID, 
+          false, 
+          "The maximum number of attempts to make before giving up on "
+          + "a UID assignment and return a RETRY error when in the "
+          + "random ID mode. (This is usually higher than the normal "
+          + "mode as random IDs can collide more often.)");
+    }
+    
+    if (!data_store.tsdb().getConfig().hasProperty(
+        data_store.getConfigKey(ATTEMPTS_KEY))) {
+      data_store.tsdb().getConfig().register(
+          data_store.getConfigKey(ATTEMPTS_KEY), 
+          DEFAULT_ATTEMPTS_ASSIGN_ID, 
+          false, 
+          "The maximum number of attempts to make before giving up on "
+          + "a UID assignment and return a RETRY error.");
+    }
   }
   
 }
