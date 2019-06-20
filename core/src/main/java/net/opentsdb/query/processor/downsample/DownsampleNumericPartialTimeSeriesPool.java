275797a5e4786c9d6f0314f93b8df997f7577b79
Chris Larsen <clarsen@verizonmedia.com>
2019-06-20 14:21:47 -0700

    Add pooAdd pooll

---
+++ core/src/main/java/net/opentsdb/query/processor/downsample/DownsampleNumericPartialTimeSeriesPool.java
@@ -0,0 +1,55 @@
+package net.opentsdb.query.processor.downsample;
+
+import com.google.common.base.Strings;
+import com.google.common.reflect.TypeToken;
+import com.stumbleupon.async.Deferred;
+
+import net.opentsdb.core.TSDB;
+import net.opentsdb.pools.BaseObjectPoolAllocator;
+import net.opentsdb.pools.DefaultObjectPoolConfig;
+import net.opentsdb.pools.ObjectPoolConfig;
+
+public class DownsampleNumericPartialTimeSeriesPool extends BaseObjectPoolAllocator {
+  public static final String TYPE = "DownsamplePartialTimeSeriesSet";
+  public static final TypeToken<?> TYPE_TOKEN = TypeToken.of(
+      DownsamplePartialTimeSeriesSet.class);
+
+  @Override
+  public String type() {
+    return TYPE;
+  }
+  
+  @Override
+  public Deferred<Object> initialize(final TSDB tsdb, final String id) {
+    if (Strings.isNullOrEmpty(id)) {
+      this.id = TYPE;
+    } else {
+      this.id = id;
+    }
+    
+    registerConfigs(tsdb.getConfig(), TYPE);
+    final ObjectPoolConfig config = DefaultObjectPoolConfig.newBuilder()
+        .setAllocator(this)
+        .setInitialCount(tsdb.getConfig().getInt(configKey(COUNT_KEY, TYPE)))
+        .setMaxCount(tsdb.getConfig().getInt(configKey(COUNT_KEY, TYPE)))
+        .setId(this.id)
+        .build();
+    try {
+      createAndRegisterPool(tsdb, config, TYPE);
+      return Deferred.fromResult(null);
+    } catch (Exception e) {
+      return Deferred.fromError(e);
+    }
+  }
+  
+  @Override
+  public Object allocate() {
+    return new DownsamplePartialTimeSeriesSet();
+  }
+  
+  @Override
+  public TypeToken<?> dataType() {
+    return TYPE_TOKEN;
+  }
+
+}
